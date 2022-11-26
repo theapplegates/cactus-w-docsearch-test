388070b4b342cca75800bcaf5dd2668a6211eeee
Paul Applegate <paul@theapplegates.com>
2022-11-26 12:24:54 -0500

    first commit

---
+++ src/site-meta.config.ts
@@ -0,0 +1,11 @@
+const siteMeta = {
+  title: "Living Life",
+  description: "Follow my daily hijinks here. A lot of science-related stuff, and some serious subjects like cancer. Fuck cancer.",
+  lang: "en-US",
+  ogLocale: "en_US",
+  githubUrl: "https://github.com/theapplegates/astro-cactus",
+  themeColorLight: "#fafafa",
+  themeColorDark: "#1d1f21",
+}
+
+export default siteMeta;
